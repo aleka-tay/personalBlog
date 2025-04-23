f4167c4387f0a2a81eeb3e316f4e3b789fe7fcc6
aleka-tay <almarwa.amirtay@gmail.com>
2025-04-23 21:56:39 +0200

    update index page, header, footer

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 import express from "express";
 import ejs from "ejs";
 
+// Variables
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
+// Route handler
+app.get("/", (req, res) => {
+    res.render("index");
+  });
+
+app.get("/createPost", (req, res) => {
+    res.render("createPost");
+});
+
+app.get("/editPost", (req, res) => {
+    res.render("editPost");
+});
+
+
+// Listening
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
