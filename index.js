fd4009993cfee85bd90471336b4db360bd7adb83
aleka-tay <almarwa.amirtay@gmail.com>
2025-04-25 10:48:42 +0200

    Backend: route to createPost

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import ejs from "ejs";
 
+
+
 // Variables
 const app = express();
 const port = 3000;
 
+let posts = [];
+
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.set("view engine", "ejs");
+
+app.set("view engine", "ejs"); //another way of rendering an ejs file
 
 // Route handler
 app.get("/", (req, res) => {
@@ -21,6 +27,27 @@ app.get("/editPost", (req, res) => {
     res.render("editPost");
 });
 
+// Post requests
+app.post("/submit", (req, res) => {
+    posts.push([req.body["title"], req.body["content"]]);
+    res.render("index", {posts: posts});
+});
+
+app.post("/submit-edit", (req, res) => {
+    console.log(req.body.num);
+    res.render("editPost", {title: req.body.title, content: req.body.content, num: req.body.num});
+});
+
+app.post("/submit-save", (req, res) => {
+    const i = req.body.num;
+    posts[i][0] = req.body.title;
+    posts[i][1] = req.body.content;
+    res.render("index", {posts: posts});
+});
+
+app.post("/submit-delete", (req, res) => {
+
+});
 
 // Listening
 app.listen(port, () => {
